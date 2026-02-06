fb88102bcba519727dcbb06173c25f34bf5154f4
jurgendl <jurgen.de.landsheer@gmail.com>
2026-02-06 22:37:37 +0100

    vite react fomantic electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import {type Dispatch, type SetStateAction, useEffect, useRef, useState} from 'react';
 import './App.scss';
+import styles from './App.module.scss';
 
 function App() {
 	return (
 		<>
+			<button className="ui button">
+				Hello world!
+			</button>
 		</>
 	);
 }
